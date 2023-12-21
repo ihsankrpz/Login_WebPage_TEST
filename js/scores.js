c25c9c750623208f84b0c3707b5c6aa1b2c6386f
lostmart <martinpedraza1979@hmail.com>
2023-12-21 12:42:49 +0100

    reame edits

--- js/scores.js
+++ js/scores.js
@@ -1,30 +1,21 @@
 const scores = JSON.parse(localStorage.getItem("scores"))
+const scoresArray = Object.entries(scores)
+
+const writeRows = () => {
+	return scoresArray
+		.map((score, i) => {
+			return `<tr>
+                <th scope="row">${i + 1}</th>
+                <td>${score[0]}</td>
+                <td>${score[1]}</td>
+                 
+            </tr>`
+		})
+		.join(" ")
+}
 
 if (!scores) {
 	console.log("error")
 } else {
-    const scoresArray = Object.entries(scores)
-    document.querySelector("tbody").innerHTML += `
-        <tr>
-            <th scope="row">1</th>
-            <td>${scoresArray[0][0]}</td>
-            <td>${scoresArray[0][1]}</td>           
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>${scoresArray[1][0]}</td>
-            <td>${scoresArray[1][1]}</td>           
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>${scoresArray[2][0]}</td>
-            <td>${scoresArray[2][1]}</td>           
-        </tr>
-        <tr>
-            <th scope="row">4</th>
-            <td>${scoresArray[3][0]}</td>
-            <td>${scoresArray[3][1]}</td>           
-        </tr>
-        `
-	
+	document.querySelector("tbody").innerHTML += writeRows()
 }
